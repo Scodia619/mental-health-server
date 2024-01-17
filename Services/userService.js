52842e0e02039b1bb64942830a7b3772c74f7799
Will <billyjoe2701@gmail.com>
2024-01-17 23:49:01 +0000

    Adds getting users by id as a route

--- Services/userService.js
+++ Services/userService.js
@@ -1,82 +1,129 @@
-const { selectAllUsers, selectUserByName, selectUsersByUsername, selectUsersByEmail, postUser, selectUserByUserAndPass, selectUsersByUsernameLogin } = require("../Repositories/userRepository")
-const { noUserError, missingDataError, incorrectDataError, usernameExistsError, emailExistsError, invalidPasswordError } = require("../errorVariables")
+const {
+  selectAllUsers,
+  selectUserByName,
+  selectUsersByUsername,
+  selectUsersByEmail,
+  postUser,
+  selectUserByUserAndPass,
+  selectUsersByUsernameLogin,
+  selectUserById,
+} = require("../Repositories/userRepository");
+const {
+  noUserError,
+  missingDataError,
+  incorrectDataError,
+  usernameExistsError,
+  emailExistsError,
+  invalidPasswordError,
+} = require("../errorVariables");
 
-const passwordHash = require('password-hash')
+const passwordHash = require("password-hash");
 
 exports.getUsers = (req, res, next) => {
-  selectAllUsers().then((users)=>{
-    res.status(200).send({users})
-  }).catch(err => next(err))
-}
+  selectAllUsers()
+    .then((users) => {
+      res.status(200).send({ users });
+    })
+    .catch((err) => next(err));
+};
 
 exports.getUsersByName = (req, res, next) => {
-    const {letters} = req.params
-    selectUserByName(letters).then((users) =>{
-        if(users.length === 0){
-            throw noUserError
-        }
-        res.status(200).send({users})
-    }).catch(err => {
-        next(err)
+  const { letters } = req.params;
+  selectUserByName(letters)
+    .then((users) => {
+      if (users.length === 0) {
+        throw noUserError;
+      }
+      res.status(200).send({ users });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
 
-exports.createUser = (req,res,next) => {
-
-    const {username, first_name, last_name, email, phone, password} = req.body
-
-    if(!username || !first_name || !last_name || !email || !phone || !password){
-        throw missingDataError
-    }
-
-    const hashedPassword = passwordHash.generate(password)
-
-    if(!isNaN(parseInt(username)) || !isNaN(parseInt(first_name)) || !isNaN(parseInt(last_name)) || !isNaN(parseInt(email))){
-        throw incorrectDataError
-    }
-
-    selectUsersByUsername(username).then((users)=>{
-        if(users){
-            throw usernameExistsError
-        }
-        return selectUsersByEmail(email)
-    }).then((users) =>{
-        if(users) {
-            throw emailExistsError
-        }
-        const userData = {
-            username,
-            first_name,
-            last_name,
-            email,
-            phone,
-            password: hashedPassword
-        }
-        return postUser(userData)
-    }).then((users)=>{
-        res.status(201).send({users})
-    }).catch(err => {
-        next(err)
+exports.getUsersById = (req, res, next) => {
+  const { id } = req.params;
+  if (isNaN(parseInt(id))) {
+    throw incorrectDataError;
+  }
+  selectUserById(id)
+    .then((users) => {
+      if (!users) {
+        throw noUserError;
+      }
+      res.status(200).send({ users });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.createUser = (req, res, next) => {
+  const { username, first_name, last_name, email, phone, password } = req.body;
+
+  if (!username || !first_name || !last_name || !email || !phone || !password) {
+    throw missingDataError;
+  }
+
+  const hashedPassword = passwordHash.generate(password);
+
+  if (
+    !isNaN(parseInt(username)) ||
+    !isNaN(parseInt(first_name)) ||
+    !isNaN(parseInt(last_name)) ||
+    !isNaN(parseInt(email))
+  ) {
+    throw incorrectDataError;
+  }
+
+  selectUsersByUsername(username)
+    .then((users) => {
+      if (users) {
+        throw usernameExistsError;
+      }
+      return selectUsersByEmail(email);
+    })
+    .then((users) => {
+      if (users) {
+        throw emailExistsError;
+      }
+      const userData = {
+        username,
+        first_name,
+        last_name,
+        email,
+        phone,
+        password: hashedPassword,
+      };
+      return postUser(userData);
+    })
+    .then((users) => {
+      res.status(201).send({ users });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.loginUser = (req, res, next) => {
-    const {username, password} = req.body
+  const { username, password } = req.body;
 
-    if(!username || !password){
-        throw missingDataError
-    }
+  if (!username || !password) {
+    throw missingDataError;
+  }
 
-    selectUsersByUsername(username).then((users) => {
-        if(!users){
-            throw noUserError
-        }
+  selectUsersByUsername(username)
+    .then((users) => {
+      if (!users) {
+        throw noUserError;
+      }
 
-        if(!passwordHash.verify(password, users.password)){
-            throw invalidPasswordError
-        }
-        res.status(200).send({users})
-    }).catch(err => {
-        next(err)
+      if (!passwordHash.verify(password, users.password)) {
+        throw invalidPasswordError;
+      }
+      res.status(200).send({ users });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,9 @@
-const { getUsers, getUsersByName, createUser, loginUser } = require('../Services/userService')
+const { getUsers, getUsersByName, createUser, loginUser, getUsersById } = require('../Services/userService')
 
 const userRouter = require('express').Router()
 
 userRouter.route('/').get(getUsers)
+userRouter.route('/id/:id').get(getUsersById)
 userRouter.route('/:letters').get(getUsersByName)
 userRouter.route('/create').post(createUser)
 userRouter.route('/login').post(loginUser)
